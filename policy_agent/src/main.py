de82a955abd4211e3d5c135c3dccd3cf60baefcf
Pablo Periañez <pabpercab1@alum.us.es>
2025-06-22 13:55:24 +0100

    feat: main

--- policy_agent/src/main.py
+++ policy_agent/src/main.py
@@ -60,7 +60,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Or specify your frontend URL, e.g., ["http://localhost:3000"]
+    allow_origins=["*"], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
